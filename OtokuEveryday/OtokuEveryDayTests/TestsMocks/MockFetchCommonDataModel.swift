35432bfa41891e08b36451c8653509e775b7c621
AkiraUeda0523 <a.ueda0523@gmail.com>
2023-12-20 21:17:52 +0900

    最新Ver 2023 12/20

---
+++ OtokuEveryday/OtokuEveryDayTests/TestsMocks/MockFetchCommonDataModel.swift
@@ -0,0 +1,8 @@
+//
+//  MockFetchCommonDataModel.swift
+//  OtokuEveryday
+//
+//  Created by 上田晃 on 2023/12/20.
+//
+
+import Foundation
---
+++ OtokuEveryday/OtokuEveryDayTests/TestsMocks/MockGeocoder.swift
@@ -0,0 +1,8 @@
+//
+//  MockGeocoder.swift
+//  OtokuEveryday
+//
+//  Created by 上田晃 on 2023/12/20.
+//
+
+import Foundation
